61e18d804e1a166981837a6b0abc183233c26645
YaelOswaldoTrejoTorres <yaeloswaldott@outlook.com>
2026-01-14 14:31:08 -0600

    Agregar archivos de proyecto.

---
+++ PL/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola esta es mi primer prueba de GitHub");
+       
+            Console.WriteLine("BIENVENIDO A LA CALCULADORA");
+            Console.WriteLine("");
+            Console.WriteLine("Ingresa el número (1) para SUMA");
+          
+            Calculadora calculadora = new Calculadora();
+
+
+            int Operaciones = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("INGRESA EL PRIMER NÚMERO:");
+            calculadora.Numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("INGRESA EL SEGUNDO NÚMERO:");
+            calculadora.Numero2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (Operaciones)
+            {
+                case 1:
+                    resultado = calculadora.Suma(calculadora.Numero1, calculadora.Numero2);
+                    break;
+                
+                default:
+                    Console.WriteLine("OPERACIÓN FALLIDA");
+                    return;
+            }
+
+            Console.WriteLine("EL RESULTADO ES: " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+
+
+
+ 
+    public class Calculadora
+    {
+
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+
+        public double Suma(double numero1, double numero2)
+        {
+            return numero1 + numero2;
+        }
+      
+    }
+}
---
+++ PL/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("PL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PL")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e20dc241-b6a1-401e-95e5-a4111ddc4072")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
